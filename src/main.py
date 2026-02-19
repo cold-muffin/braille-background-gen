ac6b0a16a9e7f3acef1e7ae66e592775de3f5ab2
Efrat Hossain <efrathossain1130@gmail.com>
2026-02-19 12:04:48 -0500

    Add cache file creation

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import math
 from dotenv import load_dotenv
 from os import getenv
 import sys
+from pathlib import Path
 
 from .processor import autocrop, read_img, get_dim, get_scale, get_color_outp
 
@@ -28,6 +29,7 @@ if __name__ == "__main__":
     DITHER=getenv("DITHER")
     THRESHOLD=getenv("TRESHOLD")
     INVERT=getenv("INVERT")
+    MEMOIZATION=getenv("MEMOIZATION")
 
     if not PATH_TO_IMAGE_FILE:
         raise ValueError("No path to image file specified")
@@ -42,6 +44,20 @@ if __name__ == "__main__":
         invert = True
     else:
         invert = False
+    
+    if MEMOIZATION and MEMOIZATION not in ["true", "false"]:
+        raise ValueError("Got INVERT as non-boolean value")
+    elif MEMOIZATION == "true":
+        memoization = True
+    else:
+        memoization = False
+
+    if memoization:
+        cache_path = Path(f"data/{".".join(file_path.split("/")[-1].split(".")[:-1])}")
+        if not cache_path.exists():
+            #print("Creating cache path...")
+            with open(cache_path, "a") as f:
+                f.write("\n".join([file_path, str(dither), str(threshold), str(invert), str(terminal_width), str(terminal_height)]))
 
     read_path = f"data/{file_path.split("/")[-1]}"
     ascii_width = terminal_width
@@ -55,7 +71,7 @@ if __name__ == "__main__":
     if projected_ascii_height > terminal_height:
         ascii_width = math.floor(ascii_width * (terminal_height/projected_ascii_height))
 
-    raw = convert(read_path, ascii_width, dither, 254, invert)
+    raw = convert(read_path, ascii_width, dither, threshold, invert)
     ascii_lines = [line for line in raw.split("\n") if line.strip()] # Remove trailing empty lines
     ascii_height = len(ascii_lines)
 
