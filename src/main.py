057c0bd477c5e8de39a6bd9490ca9963e25032f0
Efrat Hossain <efrathossain1130@gmail.com>
2026-02-19 12:12:00 -0500

    Add cache printing

--- src/main.py
+++ src/main.py
@@ -16,66 +16,77 @@ def convert(path, width, dither, threshold, invert):
     return result.stdout
 
 if __name__ == "__main__":
-    if len(sys.argv) >= 3:
-        terminal_width = int(sys.argv[1])
-        terminal_height = int(sys.argv[2])-3
-    else:
-        raise ValueError("Please specify terminal width and height")
+    def main():
+        if len(sys.argv) >= 3:
+            terminal_width = int(sys.argv[1])
+            terminal_height = int(sys.argv[2])-3
+        else:
+            raise ValueError("Please specify terminal width and height")
 
-    # Read .env
-    load_dotenv()
+        # Read .env
+        load_dotenv()
 
-    PATH_TO_IMAGE_FILE=getenv("PATH_TO_IMAGE_FILE")
-    DITHER=getenv("DITHER")
-    THRESHOLD=getenv("TRESHOLD")
-    INVERT=getenv("INVERT")
-    MEMOIZATION=getenv("MEMOIZATION")
+        PATH_TO_IMAGE_FILE=getenv("PATH_TO_IMAGE_FILE")
+        DITHER=getenv("DITHER")
+        THRESHOLD=getenv("TRESHOLD")
+        INVERT=getenv("INVERT")
+        MEMOIZATION=getenv("MEMOIZATION")
 
-    if not PATH_TO_IMAGE_FILE:
-        raise ValueError("No path to image file specified")
+        if not PATH_TO_IMAGE_FILE:
+            raise ValueError("No path to image file specified")
 
-    file_path = PATH_TO_IMAGE_FILE
-    dither = DITHER or "floydSteinberg"
-    threshold = int(THRESHOLD or "100")
+        file_path = PATH_TO_IMAGE_FILE
+        dither = DITHER or "floydSteinberg"
+        threshold = int(THRESHOLD or "100")
 
-    if INVERT and INVERT not in ["true", "false"]:
-        raise ValueError("Got INVERT as non-boolean value")
-    elif INVERT == "true":
-        invert = True
-    else:
-        invert = False
-    
-    if MEMOIZATION and MEMOIZATION not in ["true", "false"]:
-        raise ValueError("Got INVERT as non-boolean value")
-    elif MEMOIZATION == "true":
-        memoization = True
-    else:
-        memoization = False
+        if INVERT and INVERT not in ["true", "false"]:
+            raise ValueError("Got INVERT as non-boolean value")
+        elif INVERT == "true":
+            invert = True
+        else:
+            invert = False
+        
+        if MEMOIZATION and MEMOIZATION not in ["true", "false"]:
+            raise ValueError("Got INVERT as non-boolean value")
+        elif MEMOIZATION == "true":
+            memoization = True
+        else:
+            memoization = False
 
-    if memoization:
         cache_path = Path(f"data/{".".join(file_path.split("/")[-1].split(".")[:-1])}")
-        if not cache_path.exists():
-            #print("Creating cache path...")
-            with open(cache_path, "a") as f:
-                f.write("\n".join([file_path, str(dither), str(threshold), str(invert), str(terminal_width), str(terminal_height)]))
+        if memoization:
+            if cache_path.exists():
+                print("exists, printing...")
+                with open(cache_path, "r") as f:
+                    print("\n".join(f.read().split("\n")[7:]))
+                return
 
-    read_path = f"data/{file_path.split("/")[-1]}"
-    ascii_width = terminal_width
+        read_path = f"data/{file_path.split("/")[-1]}"
+        ascii_width = terminal_width
 
-    autocrop(file_path, read_path)
+        autocrop(file_path, read_path)
 
-    img = read_img(read_path)
-    dim_x, dim_y = get_dim(img)
+        img = read_img(read_path)
+        dim_x, dim_y = get_dim(img)
 
-    projected_ascii_height = math.ceil(ascii_width/(dim_x*2/dim_y))
-    if projected_ascii_height > terminal_height:
-        ascii_width = math.floor(ascii_width * (terminal_height/projected_ascii_height))
+        projected_ascii_height = math.ceil(ascii_width/(dim_x*2/dim_y))
+        if projected_ascii_height > terminal_height:
+            ascii_width = math.floor(ascii_width * (terminal_height/projected_ascii_height))
 
-    raw = convert(read_path, ascii_width, dither, threshold, invert)
-    ascii_lines = [line for line in raw.split("\n") if line.strip()] # Remove trailing empty lines
-    ascii_height = len(ascii_lines)
+        raw = convert(read_path, ascii_width, dither, threshold, invert)
+        ascii_lines = [line for line in raw.split("\n") if line.strip()] # Remove trailing empty lines
+        ascii_height = len(ascii_lines)
 
-    scale_x, scale_y = get_scale(dim_x, dim_y, ascii_width, ascii_height)
-    color_outp = get_color_outp(scale_x, scale_y, dim_x, dim_y, ascii_lines, img)
+        scale_x, scale_y = get_scale(dim_x, dim_y, ascii_width, ascii_height)
+        color_outp = get_color_outp(scale_x, scale_y, dim_x, dim_y, ascii_lines, img)
 
-    print("\n".join(color_outp))
+        outp = "\n".join(color_outp)
+        print(outp)
+
+        if memoization:
+            if not cache_path.exists():
+                #print("Creating cache path...")
+                with open(cache_path, "a") as f:
+                    f.write("\n".join([file_path, str(dither), str(threshold), str(invert), str(terminal_width), str(terminal_height), outp]))
+
+    main()
